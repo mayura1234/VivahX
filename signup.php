e57dc4b04277d89ec8f386ec0a6e24e450a5db01
ChandanMayya <chandanmogral@gmail.com>
2022-09-09 23:24:30 +0530

    working with db.. sep9

--- signup.php
+++ signup.php
@@ -1,3 +1,28 @@
+<?php 
+session_start();
+
+include("connection.php");
+/*
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    $psw=($_POST['password']);
+    $rpsw=($_POST['reppass']);
+    $mail=($_POST['mail']);
+    $acnt_type=($_POST['gender']);
+}*/
+
+function dbquery1(){
+    $user_name="";
+    $user_name=($_POST['username']);
+    $query = "SELECT uname FROM user WHERE uname='$user_name'";
+    //$runquery=mysqli_query($con, $query);
+    //if(mysqli_num_rows($runquery) != 0)
+    {
+        echo("Username already exists");
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,8 +46,20 @@
     <link rel="stylesheet" href="assets/css/owl.css">
     <link rel="stylesheet" href="assets/css/animate.css">
     <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css">
-
+    <script>
+        function btnprs(){
+            var psw=document.getElementById("pw").value;
+            var rpsw=document.getElementById("rpw").value;
+            if(psw!=rpsw){
+                document.getElementById("error_msg").innerHTML="<h6 class='text-danger'>Passwords does not match!</h6><br>";
+            }else{
+                <?php      dbquery1();          ?>
+            }
+        }
+      </script>
   </head>
+ 
+    
 
 <body>
 
@@ -68,25 +105,29 @@
     <center><br><br><br><br><br><br>
     <h1> Registration Form </h1>
     <br>
-    <form class="from-group">
+    <form class="from-group" method="post">
         <label for="username">User Name:
-            <input type="text" required>
-        </label><br><br>
-        <label for="password">Password:
-            <input type="password" required>
-        </label><br><br>
-        <label for="reppass">Repeat Password:
-            <input type="password" name="" id=""required>
+            <input type="text" required name="username" id="un">
         </label><br><br>
         <label for="email">Email:
-            <input type="email" name="" id=""required>
+            <input type="email" name="email" id="mail">
         </label><br><br>
-        <label for="usertype">Gender:<br>
-            <input type="radio" name="gender" id="">Male<br>
-            <input type="radio" name="gender" id="">Female
-        </label><br><br>    
-        <input type="submit" value="Submit">
+        <label for="password">Password:
+            <input type="password" name="password" id="pw">
+        </label><br><br>
+        <label for="reppass">Repeat Password:
+            <input type="password" name="reppass" id="rpw">
+        </label><br><br>
+        <label for="gender">Gender:<br>
+            <input type="radio" name="gender" id="bg">Male<br>
+            <input type="radio" name="gender" id="b">Female
+        </label><br><br>   
+        <span id="error_msg"></span> 
+        <input type="button" value="Submit" onclick="btnprs();">
         <input type="reset" value="Reset">
     </form>
     </center>
     </div>
+    </body>
+    </html>
+    
